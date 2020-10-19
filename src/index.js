e910884d34a357cc19d0333d6b2369b39d2d1b42
Илья Богачев <Nate-Death@Yandex.ru>
2020-10-19 21:28:39 +0400

    init: start 'brackets' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let startEnd = [];
+
+        bracketsConfig.forEach(element => {
+            startEnd.push(element.join(""));
+        });
+
+        for(let i = 0; i <= startEnd.length; i++) {
+          startEnd.forEach(item => {
+              while(str.indexOf(item) != -1) {
+                str = str.replace(item, "");
+              }
+          })
+          if(str.length == 0) return true;
+        }
+        return false;
 }
